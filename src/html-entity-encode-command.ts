dc328084626da1487349aa91eb3b5380a6f6de15
Budi Irawan <deerawan@gmail.com>
2016-01-07 21:20:19 +0700

    add html entity command class with test

---
+++ src/html-entity-encode-command.ts
@@ -0,0 +1,10 @@
+import {ICommand} from './command';
+import he = require('he');
+
+export class HtmlEntityEncodeCommand implements ICommand {
+    run(text: string): string {
+        return he.encode(text, {
+            'useNamedReferences': true
+        });
+    }
+}
---
+++ test/html-entity-encode-command.test.ts
@@ -0,0 +1,10 @@
+import * as assert from 'assert';
+import {HtmlEntityEncodeCommand} from '../src/html-entity-encode-command';
+
+suite('HTML Entity Encode Tests', () => {
+    test('HTML Entity Encode', () => {
+        let command = new HtmlEntityEncodeCommand();
+        assert.equal(command.run('foo © bar ≠ baz 𝌆 qux'), 'foo &copy; bar &ne; baz &#x1D306; qux');
+        assert.equal(command.run('<body>'), '&lt;body&gt;');
+    });
+});
